39775f2b81dbcbffc0f3e369a0b3b9f8bd7a3c80
Cody Seibert <webdevcody@gmail.com>
2025-08-15 13:33:04 -0400

    Update Stripe API version from 2025-02-24.acacia to 2025-06-30.basil

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,5 +2,5 @@ import Stripe from "stripe";
 import { env } from "~/utils/env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-02-24.acacia",
+  apiVersion: "2025-06-30.basil",
 });
